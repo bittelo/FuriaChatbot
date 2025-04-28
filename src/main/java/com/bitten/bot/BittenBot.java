f5a13a90fb3fbe26f376526a50528375c5df9a77
bittelo <pedrohanza432@gmail.com>
2025-04-28 15:49:09 -0300

    feat: add mais comandos

--- src/main/java/com/bitten/bot/BittenBot.java
+++ src/main/java/com/bitten/bot/BittenBot.java
@@ -52,10 +52,22 @@ public class BittenBot extends TelegramLongPollingBot {
         } else if (textoMensagem.startsWith("/start") ||textoMensagem.startsWith("oi") || textoMensagem.startsWith("oii") || textoMensagem.startsWith("ola") || textoMensagem.startsWith("olá")){
                 resposta = "Olá Furioso! O que você precisa hoje? Você pode acessar os comandos com o /comandos";
         } else if(textoMensagem.startsWith("/proximojogo") || textoMensagem.startsWith("proximojogo")){
+                resposta ="Com base no site https://draft5.gg/equipe/330-FURIA, a FURIA ainda não tem um próximo jogo agendado.\n";
 
         } else if (textoMensagem.startsWith("/ultimojogo") || textoMensagem.startsWith("ultimojogo")){
+                resposta ="Última Partida da FURIA:\n\n" +
+                        "FURIA 0 - 2 THE MONGOLZ\n" +
+                        "PGL Bucharest 2025\n\n" +
+                        "Confira a partida: https://draft5.gg/partida/36342-FURIA-vs-The-MongolZ-PGL-Bucharest-2025";
 
-        } else if (textoMensagem.startsWith("/comandos")){
+        } else if (textoMensagem.startsWith("/comunidade") || textoMensagem.startsWith("comunidade")){
+            resposta = "Converse com outros furiosos!\nDiscord: https://discord.com/invite/furia\n";
+        } else if(textoMensagem.startsWith("/loja") || textoMensagem.startsWith("loja")){
+            resposta = "Vista o estilo FURIA: moda para quem vive o jogo\n https://www.furia.gg/";
+        } else if(textoMensagem.startsWith("/elenco") || textoMensagem.startsWith("elenco")){
+            resposta = "Atualmente o elenco da FURIA conta com:\n \uD83C\uDDF0\uD83C\uDDFFMOLODY\n \uD83C\uDDF1\uD83C\uDDFBYEKINDAR\n \uD83C\uDDE7\uD83C\uDDF7FalleN\n \uD83C\uDDE7\uD83C\uDDF7KSCERATO\n \uD83C\uDDE7\uD83C\uDDF7yuurih";
+        }
+        else if (textoMensagem.startsWith("/comandos")){
             resposta = "Aqui está uma lista de comandos: \nproximojogo\nultimojogo\ncomunidade\nloja\nelenco";
         } else {
             resposta = "Não entendi!\\nDigite /help para ver os comandos disponíveis.";
@@ -66,6 +78,7 @@ public class BittenBot extends TelegramLongPollingBot {
                 .chatId(chatId)
                 .build();
     }
+
 
     private String getData() {
         var formatter = new SimpleDateFormat("dd/MM/yyyy");
