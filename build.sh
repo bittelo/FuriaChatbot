cda9e1770469650f65018f42e38ba8604276a062
bittelo <pedrohanza432@gmail.com>
2025-04-29 23:42:38 -0300

    fix: removed

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Setting up Java environment..."
-export JAVA_HOME=/opt/java/openjdk
-export PATH=$PATH:$JAVA_HOME/bin
-echo "Java version:"
-java -version
-echo "Running Maven build..."
-chmod +x ./mvnw
-./mvnw clean package
